e8ca9f49c740e9a13745cd96dc5ed5d4cfdd1c40
Steve Leach <sleach@wayfair.com>
2019-06-07 10:59:11 -0400

    added a bunch of color stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Admin, Resource } from "react-admin";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
@@ -9,32 +9,43 @@ import UserIcon from "@material-ui/icons/Group";
 import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
-console.log(dataProvider);
-const App = () => (
-  <Admin
-    dashboard={Dashboard}
-    authProvider={authProvider}
-    dataProvider={dataProvider}
-  >
-    <Resource
-      name="posts"
-      list={PostList}
-      edit={PostEdit}
-      create={PostCreate}
-      icon={PostIcon}
-    />
-    <Resource name="users" list={UserList} icon={UserIcon} />
-  </Admin>
-);
-const sassExtract = require("sass-extract");
 
-sassExtract
-  .render({
-    file: "./sass/_global_vars.scss"
-  })
-  .then(rendered => {
-    console.log(rendered.vars);
-    console.log(rendered.css.toString());
-  });
+const App = () => {
+  /*const [brand, setBrand] = useState("wf");
+  const toggleBrand = () => {
+    return brand === "wf" ? setBrand("joss") : setBrand("wf");
+  };*/
+
+  const brands = ["wf", "joss", "seattle"];
+  return (
+    <>
+      <Admin
+        dashboard={Dashboard}
+        authProvider={authProvider}
+        dataProvider={dataProvider}
+      >
+        <Resource
+          name="posts"
+          list={PostList}
+          edit={PostEdit}
+          create={PostCreate}
+          icon={PostIcon}
+        />
+        <Resource name="users" list={UserList} icon={UserIcon} />
+      </Admin>
+      <div>
+        {brands.map(brand => {
+          const variables = require(`./sass/${brand}/global_vars.scss`);
+          console.log(variables);
+          return Object.entries(variables).map((color, i) => (
+            <div key={i} style={{ background: color[1], color: "white" }}>
+              {color[0]}: {color[1]}
+            </div>
+          ));
+        })}
+      </div>
+    </>
+  );
+};
 
 export default App;
